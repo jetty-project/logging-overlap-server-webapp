638a2f073eba326d4a717181abd8e458fd509346
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2020-10-12 09:59:57 -0500

    Adding license/copyright

--- src/main/java/org/eclipse/jetty/demo/NaughtyServlet.java
+++ src/main/java/org/eclipse/jetty/demo/NaughtyServlet.java
@@ -1,8 +1,24 @@
+//
+//  ========================================================================
+//  Copyright (c) Mort Bay Consulting Pty Ltd and others.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
 package org.eclipse.jetty.demo;
 
-import java.io.IOException;
 import java.nio.charset.UnsupportedCharsetException;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +26,7 @@ import javax.servlet.http.HttpServletResponse;
 public class NaughtyServlet extends HttpServlet
 {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
     {
         // Intentionally cause an error 500.
         throw new UnsupportedCharsetException("BAD");
